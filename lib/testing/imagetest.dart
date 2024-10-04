7d037616c95e28736651439dadc735f71f006e52
jl <orionjl1106@gmail.com>
2024-10-05 00:36:30 +0800

    save image post final

--- lib/testing/imagetest.dart
+++ lib/testing/imagetest.dart
@@ -1,10 +1,9 @@
-
-
 // ignore_for_file: prefer_const_constructors
 
 import 'dart:io';
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/foundation.dart';
@@ -23,99 +22,114 @@ class Imagetest extends StatefulWidget {
 }
 
 class _ImagetestState extends State<Imagetest> {
-
-    XFile? _image;
-    final List<Product> product = ProductSeeder().productListSeed();
-    final TextEditingController _name = TextEditingController();
-    final TextEditingController _selling_price = TextEditingController();
-    final TextEditingController _total_purchase = TextEditingController();
-    final TextEditingController _product_stock = TextEditingController();
-    final TextEditingController _category = TextEditingController();
-    String _finalImage = "";
+  XFile? _image;
+  final List<Product> product = ProductSeeder().productListSeed();
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _selling_price = TextEditingController();
+  final TextEditingController _total_purchase = TextEditingController();
+  final TextEditingController _product_stock = TextEditingController();
+  final TextEditingController _category = TextEditingController();
+  String _finalImage = "";
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    
-
   }
-  
-  
 
   @override
   Widget build(BuildContext context) {
-
     return Consumer<StorageService>(
       builder: (context, storageService, widget) => Scaffold(
         floatingActionButton: FloatingActionButton(
-          onPressed: () => setState(() async{
+          onPressed: () => setState(() async {
             _image = await storageService.selectImage();
           }),
           child: const Icon(Icons.add),
-          ),
-          body: ListView(
-            children: [
-              TextField(
-                controller: _name,
-                decoration: InputDecoration(labelText: 'NAME'),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                controller: _selling_price,
-                decoration: InputDecoration(labelText: 'Selling Price'),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                controller: _total_purchase,
-                decoration: InputDecoration(labelText: 'Total Purchase'),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                controller: _product_stock,
-                decoration: InputDecoration(labelText: 'Product Stock'),
-              ),
-              SizedBox(height: 10,),
-              TextField(
-                controller: _category,
-                decoration: InputDecoration(labelText: 'Category'),
-              ),
-              SizedBox(height: 10,),
-              //abstract this to a function becuase fuck this piece of shit 
-              _image != null
+        ),
+        body: ListView(
+          children: [
+            TextField(
+              controller: _name,
+              decoration: InputDecoration(labelText: 'NAME'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _selling_price,
+              decoration: InputDecoration(labelText: 'Selling Price'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _total_purchase,
+              decoration: InputDecoration(labelText: 'Total Purchase'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _product_stock,
+              decoration: InputDecoration(labelText: 'Product Stock'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _category,
+              decoration: InputDecoration(labelText: 'Category'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            //abstract this to a function becuase fuck this piece of shit
+            _image != null
                 ? kIsWeb
-                ? Image.network(_image!.path)
-                : Image.file(File(_image!.path))
+                    ? Image.network(_image!.path)
+                    : Image.file(File(_image!.path))
                 : Text('No Image Selected'),
-              Center(
-                child: ElevatedButton(
-                  onPressed: ()async {
-                    //this is selec image 
+            Center(
+              child: ElevatedButton(
+                  onPressed: () async {
+                    //this is selec image
                     final imageSelected = await storageService.selectImage();
-                    setState((){
+                    setState(() {
                       _image = imageSelected;
                     });
-                  }, 
+                  },
                   child: Icon(Icons.post_add)),
-              ),
-              SizedBox(height: 20,),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () async{
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: ElevatedButton(
+                  onPressed: () async {
                     //upload image and get link
-                    _finalImage = await storageService.updloadImage(_image); 
+                    _finalImage = await storageService.updloadImage(_image);
+                    CollectionReference ref = FirebaseFirestore.instance.collection('products');
                     //upload to firestore with Prdouct Model
-                    }, 
+                    Product prodPost = Product(
+                        id: '',
+                        name: _name.text,
+                        selling_price: double.parse(_selling_price.text),
+                        total_purchase: double.parse(_total_purchase.text),
+                        product_stock: int.parse(_product_stock.text),
+                        category: _category.text,
+                        image: _finalImage);
+                    DocumentReference docref  = await ref.add(prodPost.toJson());
+                    await docref.update({
+                      'id': docref.id,
+                    });
+                  },
                   child: Icon(Icons.send)),
-              )
-              
-              
-            ],
-          
-          ),
-            //Image.network(imageurl)
-          ),
-      );
-    
+            )
+          ],
+        ),
+        //Image.network(imageurl)
+      ),
+    );
   }
 }
